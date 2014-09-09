2db4c1756fc4495dc191d0113d597752d39b28da
Pyry-Samuli Lahti <pyppe@iki.fi>
2014-09-10 00:09:25 +0300

    Show received user JSON on index.scala.html

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,12 +4,17 @@ import fi.pyppe.simpleauth.{UserResponse, Auth}
 import play.api._
 import play.api.mvc._
 import play.api.Play.current
+import play.api.cache.Cache
+import play.api.libs.json.Json
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
   def index = Action { implicit req =>
-    Ok(views.html.index())
+    val userJson =
+      req.session.get("user.identity").
+        flatMap(Cache.getAs[String])
+    Ok(views.html.index(userJson))
   }
 
   def authenticate(provider: String) = Action.async { implicit req =>
@@ -21,15 +26,18 @@ object Application extends Controller {
       /*
        HERE WE WOULD TYPICALLY SAVE THE USER DETAILS TO A DATABASE
        */
+      val identity = user.identity.toString
+      // A bit hacky: let's store JSON temporarily in cache so we can show it after redirect
+      Cache.set(identity, Json.prettyPrint(userJson), 60*10)
+
       val session = Session(Seq(
         Some("user.identity" -> user.identity.toString),
         Some("user.name" -> user.name),
         user.email.map(e => "user.email" -> e),
         user.image.map(i => "user.image" -> i)
       ).flatten.toMap)
-      Redirect("/").
-        withSession(session).
-        flashing("userJson" -> userJson.toString)
+
+      Redirect("/").withSession(session)
     }
   }
 
--- build.sbt
+++ build.sbt
@@ -9,6 +9,7 @@ scalaVersion := "2.11.1"
 resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
 
 libraryDependencies ++= Seq(
+  cache,
   ws,
   "fi.pyppe" %% "play-simple-auth" % "0.1-SNAPSHOT" changing()
 )
